5f8f724e853ceaa43824c66b7cd5b38f834d31da
Valentin Favard <lefav@MacBook-Pro-de-Valentin.local>
2025-06-16 15:18:17 +0200

    recup map

--- check_map.c
+++ check_map.c
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:43:25 by vafavard          #+#    #+#             */
-/*   Updated: 2025/06/16 15:07:10 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:17:54 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,3 +34,42 @@ int nb_line(char *file)
 	return (count);
 }
 
+char	**load_map(char *file)
+{
+	int		fd;
+	int		lines;
+	int		i = 0;
+	char	**map;
+	char	*line;
+	
+	lines = nb_line(file);
+	if (lines <= 0)
+		return (NULL);
+	map = malloc(sizeof(char *) * (lines + 1));
+	if (!map)
+		return (NULL);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	while (line = get_next_line(fd))
+	{
+		map[i] = line;
+		i++;
+	}
+	map[i] = NULL;
+	close(fd);
+	return (map);
+}
+
+void	free_map(char **map)
+{
+	int	i;
+	
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
