4e44503815624c4b3e8b09aebe28cf729c67af1b
Valentin Favard <lefav@MacBook-Pro-de-Valentin.local>
2025-07-09 04:48:58 +0200

    image bonus

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:28:26 by vafavard          #+#    #+#             */
-/*   Updated: 2025/07/09 03:25:44 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/07/09 04:30:56 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,9 @@ typedef struct s_textures_bonus
 {
 	void	*villain_image;
 	void	*hurt_player;
+	void	*zero;
 	void	*one;
+	void	*two;
 	void	*three;
 	void	*four;
 	void	*five;
--- sources/bonus_sources/image_bonus.c
+++ sources/bonus_sources/image_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 03:37:08 by vafavard          #+#    #+#             */
-/*   Updated: 2025/07/09 03:28:40 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/07/09 04:48:36 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,36 @@ void	render_map_bonus(t_game *game)
 	mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.heart_full, 0 * TILE_SIZE, 0 * TILE_SIZE);
 	mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.heart_full, 1 * TILE_SIZE, 0 * TILE_SIZE);
 	mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.heart_full, 2 * TILE_SIZE, 0 * TILE_SIZE);
+}
 
-
+//nouvelle fonction (lignes trop longues et limite de 25 lignes donc a voir...)
+void	render_map_number_move_bonus(t_game *game, char *str, int i)
+{
+	i = 4;
+	while (str[i])
+	{
+		if (str[i] == '0')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.zero, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '1')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.one, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '2')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.two, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '3')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.three, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '4')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.four, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '5')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.five, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '6')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.six, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '7')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.seven, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '8')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.eight, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		if (str[i] == '9')
+			mlx_put_image_to_window(game->mlx, game->win, game->tex_bonus.nine, game->map_width - i * TILE_SIZE, game->map_height * TILE_SIZE);
+		i--;
+	}
 }
 
 //faire un init_image bonus
@@ -66,8 +94,29 @@ void	init_image(t_game *game)
 	game->tex.floor_image = mlx_xpm_file_to_image(game->mlx, "texture/floor.xpm", &w, &h);
 	game->tex.player_image = mlx_xpm_file_to_image(game->mlx, "texture/player.xpm", &w, &h);
 	game->tex.wall_image = mlx_xpm_file_to_image(game->mlx, "texture/wall.xpm", &w, &h);
-	game->tex_bonus.villain_image = mlx_xpm_file_to_image(game->mlx, "texture/villain.xpm", &w, &h);
 	//villain a mettre dans bonus
+	game->tex_bonus.villain_image = mlx_xpm_file_to_image(game->mlx, "texture/villain.xpm", &w, &h);
+}
+
+void	init_image_bonus(t_game *game)
+{
+	int w;
+	int h;
+	
+	game->tex_bonus.villain_image = mlx_xpm_file_to_image(game->mlx, "texture/villain.xpm", &w, &h);
+	game->tex_bonus.zero = mlx_xpm_file_to_image(game->mlx, "texture/zero.xpm", &w, &h);
+	game->tex_bonus.one = mlx_xpm_file_to_image(game->mlx, "texture/one.xpm", &w, &h);
+	game->tex_bonus.two = mlx_xpm_file_to_image(game->mlx, "texture/two.xpm", &w, &h);
+	game->tex_bonus.three = mlx_xpm_file_to_image(game->mlx, "texture/three.xpm", &w, &h);
+	game->tex_bonus.four = mlx_xpm_file_to_image(game->mlx, "texture/four.xpm", &w, &h);
+	game->tex_bonus.five = mlx_xpm_file_to_image(game->mlx, "texture/five.xpm", &w, &h);
+	game->tex_bonus.six = mlx_xpm_file_to_image(game->mlx, "texture/six.xpm", &w, &h);
+	game->tex_bonus.seven = mlx_xpm_file_to_image(game->mlx, "texture/seven.xpm", &w, &h);
+	game->tex_bonus.eight = mlx_xpm_file_to_image(game->mlx, "texture/eight.xpm", &w, &h);
+	game->tex_bonus.nine = mlx_xpm_file_to_image(game->mlx, "texture/nine.xpm", &w, &h);
+	game->tex_bonus.heart_full = mlx_xpm_file_to_image(game->mlx, "texture/heart_full.xpm", &w, &h);
+	game->tex_bonus.heart_empty = mlx_xpm_file_to_image(game->mlx, "texture/heart_empty.xpm", &w, &h);
+	game->tex_bonus.hurt_player = mlx_xpm_file_to_image(game->mlx, "texture/player_hurt.xpm", &w, &h);
 }
 
 int    set_map_width_height(t_game *game)
